3535c97a8cc7ecd5423b5eeaf1eb6cf31453f1ff
Sebastian Martin <sebastianmartin@Sebastians-MacBook-Pro.local>
2019-10-24 15:18:35 +0200

    change HelloWorld -name

---
+++ src/components/LectureApp.vue
@@ -0,0 +1,21 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: 'LectureApp',
+  props: {
+  },
+  data() {
+    return {
+
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
