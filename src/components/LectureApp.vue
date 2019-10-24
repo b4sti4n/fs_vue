b8849d43caf295b6f5f9d3d37d667b5af0b4eb83
Sebastian Martin <sebastianmartin@Sebastians-MacBook-Pro.local>
2019-10-24 16:04:29 +0200

    start lecture app

--- src/components/LectureApp.vue
+++ src/components/LectureApp.vue
@@ -1,5 +1,8 @@
 <template>
-
+  <div class="lecture-wrapper">
+    <!-- Insert Content here -->
+    <h3>Eine Komponente (Parent)</h3>
+  </div>
 </template>
 
 <script>
@@ -17,5 +20,13 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+.lecture-wrapper {
+  width: 100%;
+  max-width: 1024px;
+  margin: 0 auto;
+  margin-top: 50px;
+  padding: 20px;
+  box-shadow: 0 3px 6px rgba(0, 0, 0, .3);
+}
 
 </style>
